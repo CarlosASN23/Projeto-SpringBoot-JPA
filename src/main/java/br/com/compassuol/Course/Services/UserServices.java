061c934704e22108c7fbf0be10c3304948e2d490
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-04-30 15:25:44 -0300

    Service layer, component registration

---
+++ src/main/java/br/com/compassuol/Course/Services/UserServices.java
@@ -0,0 +1,26 @@
+package br.com.compassuol.Course.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.compassuol.Course.Entity.User;
+import br.com.compassuol.Course.Repositories.UserRepository;
+
+@Service
+public class UserServices {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =repository.findById(id);
+		return obj.get();
+	}
+}
