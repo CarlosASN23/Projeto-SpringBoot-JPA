5b97d0777037fefba7d99184bea757c69b036a92
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-04-30 16:08:58 -0300

    Order, Instant

--- src/main/java/br/com/compassuol/Course/Entity/User.java
+++ src/main/java/br/com/compassuol/Course/Entity/User.java
@@ -1,12 +1,15 @@
 package br.com.compassuol.Course.Entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +22,10 @@ public class User implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name, email, phone, password;
+	
+	// Associações Orders
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -72,6 +79,11 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -89,5 +101,6 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 }
