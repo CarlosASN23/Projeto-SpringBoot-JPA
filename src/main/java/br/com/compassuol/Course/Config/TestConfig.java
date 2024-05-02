86158b4a3e6df7875dbac0e78a58bcc2f1c81c12
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-02 14:04:16 -0300

    Payment, one-to-one association

--- src/main/java/br/com/compassuol/Course/Config/TestConfig.java
+++ src/main/java/br/com/compassuol/Course/Config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.compassuol.Course.Entity.Category;
 import br.com.compassuol.Course.Entity.Order;
 import br.com.compassuol.Course.Entity.OrderItem;
+import br.com.compassuol.Course.Entity.Payment;
 import br.com.compassuol.Course.Entity.Product;
 import br.com.compassuol.Course.Entity.User;
 import br.com.compassuol.Course.Entity.Enums.OrderStatus;
@@ -79,6 +80,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
