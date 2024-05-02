332d9e6e55e2565dbfe29363a90660b2e2a22c8b
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-02 15:40:00 -0300

    Exception hadler

---
+++ src/main/java/br/com/compassuol/Course/Services/Exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.compassuol.Course.Services.Exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
--- src/main/java/br/com/compassuol/Course/Services/UserServices.java
+++ src/main/java/br/com/compassuol/Course/Services/UserServices.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.compassuol.Course.Entity.User;
 import br.com.compassuol.Course.Repositories.UserRepository;
+import br.com.compassuol.Course.Services.Exception.ResourceNotFoundException;
 
 @Service
 public class UserServices {
@@ -21,7 +22,7 @@ public class UserServices {
 	
 	public User findById(Long id) {
 		Optional<User>obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
