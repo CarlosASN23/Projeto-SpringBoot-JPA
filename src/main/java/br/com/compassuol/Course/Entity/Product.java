220d6b51075ef0abc85a1b05cb067b9d44aa3a5e
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-02 13:51:28 -0300

    Product orderItem one-to-many

--- src/main/java/br/com/compassuol/Course/Entity/Product.java
+++ src/main/java/br/com/compassuol/Course/Entity/Product.java
@@ -7,6 +7,8 @@ import java.util.Set;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -36,6 +39,9 @@ public class Product implements Serializable{
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 	
 	}
@@ -94,6 +100,17 @@ public class Product implements Serializable{
 		this.price = price;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
