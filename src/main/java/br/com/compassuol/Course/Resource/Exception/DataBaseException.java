08b3208eda7ee91dcd3a17231b31778347cdd67c
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-02 15:52:27 -0300

    Exceptions Handler + Database Exception

---
+++ src/main/java/br/com/compassuol/Course/Resource/Exception/DataBaseException.java
@@ -0,0 +1,11 @@
+package br.com.compassuol.Course.Resource.Exception;
+
+public class DataBaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
--- src/main/java/br/com/compassuol/Course/Services/UserServices.java
+++ src/main/java/br/com/compassuol/Course/Services/UserServices.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.compassuol.Course.Entity.User;
 import br.com.compassuol.Course.Repositories.UserRepository;
+import br.com.compassuol.Course.Resource.Exception.DataBaseException;
 import br.com.compassuol.Course.Services.Exception.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,18 @@ public class UserServices {
 	}
 	
 	public void delete(Long id) {
+		try {
+			
 		repository.deleteById(id);
+		
+		} catch(EmptyResultDataAccessException e) {
+			
+			throw new ResourceNotFoundException(id);
+			
+		} catch(DataIntegrityViolationException e) {
+			
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
